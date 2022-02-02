e0a59011e2bbe5a7906fc50e993ebe9cfaffa307
Ludovico Cammarata <60526281+frosty04@users.noreply.github.com>
2022-02-02 09:27:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
 
-const newYears = '25 Dec 2021';
+const newYears = '25 Dec 2022';
 
 function countdown () {
     const newYearsDate = new Date(newYears);
